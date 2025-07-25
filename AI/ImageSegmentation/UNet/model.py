0544058bd752a29c38d062f1c1abbcb036169721
jungrae <wjdfoek3@naver.com>
2025-07-25 17:55:50 +0900

    Fix: U-Net Runtime Error 수정
    n_channels 인자 못받는 에러 수정
    UNet/hyperparameters.py 파일에 n_channels 속성 추가
    UNet/model.py에서 하이퍼파라미터 사용하도록 수정

--- AI/ImageSegmentation/UNet/model.py
+++ AI/ImageSegmentation/UNet/model.py
@@ -54,7 +54,7 @@ class UNetLightning(BaseSegmentationModule):
         super().__init__(**kwargs)
 
         # 인코더
-        self.inc = DoubleConv(n_channels, 64)
+        self.inc = DoubleConv(self.hparams.n_channels, 64)
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
