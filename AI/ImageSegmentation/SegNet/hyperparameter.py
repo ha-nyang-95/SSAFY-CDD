e74c35f49fec73b134ad4aa9b8686a21cb595d91
jungrae <wjdfoek3@naver.com>
2025-07-25 13:25:42 +0900

    Refactor: lightning module(factory method pattern)

--- AI/ImageSegmentation/SegNet/hyperparameter.py
+++ AI/ImageSegmentation/SegNet/hyperparameter.py
@@ -9,14 +9,18 @@ HYPERPARAMETERS = {
     "num_epochs": 1,
     "batch_size": 16,
     "learning_rate": 0.0001,
-    "optimizer": "Adam",
-    "loss_function": "BCELoss", # Or "BCELoss", "DiceLoss", "DiceBCEWithLogitsLoss"
+    "optimizer": "AdamW", # AdamW 사용할 때 무조건 wegiht_decay 설정
+    "weight_decay": 1e-2,
+    "loss_function": "BCEWithLogitsLoss", # Or "BCELoss", "DiceLoss", "DiceBCEWithLogitsLoss"
     "image_size": 512,
     "num_workers": 4,
+    "num_classes": 1,
     
     # scheduler 설정
     "scheduler": "ReduceLROnPlateau", # Or None, "CosineAnnealingLR"
     "scheduler_mode": "max",
     "scheduler_patience": 5,
     "scheduler_factor": 0.5,
+    "T_max": 50,
+    "eta_min": 1e-6,
 }
