6b691c940b13caa0734258a07ad9bd778b6af04a
Hyochang098 <wlsgyckd7@naver.com>
2025-08-16 16:23:44 +0900

    fix: cors 설정 변경

--- WEB/BE/CrackTrack/src/main/java/com/b102/cracktrack/domain/security/config/SecurityConfig.java
+++ WEB/BE/CrackTrack/src/main/java/com/b102/cracktrack/domain/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.b102.cracktrack.domain.security.config;
 import com.b102.cracktrack.domain.security.handler.CustomAccessDeniedHandler;
 import com.b102.cracktrack.domain.security.handler.JwtAuthenticationEntryPoint;
 import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -44,7 +45,9 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+    configuration.setAllowedOrigins(List.of("http://localhost:5173",
+        "http://43.203.222.137",
+        "http://43.203.222.137:8081"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("*"));
     configuration.setAllowCredentials(true);
